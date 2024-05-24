1b63850b1cafcf9228411c63c6e96a4f11b024d9
Marko Bozilovic <>
2024-05-24 13:55:36 +0200

    initial project

---
+++ EagleAssignment/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  EagleAssignment
+//
+//  Created by Marko Bozilovic on 24.05.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    // Override point for customization after application launch.
+    return true
+  }
+}
