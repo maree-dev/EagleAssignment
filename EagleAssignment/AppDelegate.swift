0d64622593573ca2b13d2491d75e40f4d723d1cd
Marko Bozilovic <>
2024-05-25 12:57:54 +0200

    add alamofire and netfox

--- EagleAssignment/AppDelegate.swift
+++ EagleAssignment/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import netfox
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    NFX.sharedInstance().start()
     coordinator = AppConcreteCoordinator()
     coordinator?.start()
     
