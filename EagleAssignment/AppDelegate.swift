c107fdd3ba0298d29d4645afda61271bf233be20
Marko Bozilovic <>
2024-05-27 03:00:32 +0200

    update app delegate

--- EagleAssignment/AppDelegate.swift
+++ EagleAssignment/AppDelegate.swift
@@ -14,10 +14,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
-    
-    NFX.sharedInstance().start()
     coordinator = AppConcreteCoordinator()
     coordinator?.start()
+    
+    #if DEBUG
+    NFX.sharedInstance().start()
+    #endif
     
     return true
   }
